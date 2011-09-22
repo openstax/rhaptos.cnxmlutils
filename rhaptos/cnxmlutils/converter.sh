4c30d17d74e37c475b789fb4a9ca48ab0e29f302
Philip Schatz <phil@cnx.org>
2011-09-22 23:42:05 +0000

    Cleaned up conversion script; Added batch mode

--- rhaptos/cnxmlutils/converter.sh
+++ rhaptos/cnxmlutils/converter.sh
@@ -10,17 +10,20 @@ if [ "." = ".${1}" ]; then
   echo "--------------------------------------"
 fi
 
-VERBOSE="" # Set to "-v" for additional debugging lines
+# When more than 1 file is provided run in batch mode (less printing)
+[ '.' == ".${2}" ]
+BATCH=$?
 
-# If more than one argument, use a temp dir
-if [ '.' != ".${2}" ]; then
-  echo "Creating temp dir for batch conversion"
+if [ ${BATCH} -ne 0 ]; then
   TMP_DIR=$(mktemp -d -t 'odt2cnxml')
+  STDERR=${TMP_DIR}/stderr.txt
 else
   TMP_DIR=$(pwd)
+  STDERR="/dev/stderr"
   VERBOSE="-v"
 fi
 
+# Open Office is only needed if the file extension isn't "odt"
 OOFFICE_BIN='/Applications/OpenOffice.org.app/Contents/MacOS/soffice'
 OOFFICE_MACRO='macro:///Standard.Module1.SaveAsOOO'
 #' The content of the Macro is as follows
@@ -58,19 +61,19 @@ WF_ORIG=${TMP_DIR}/wf-orig.txt
 WF_CONV=${TMP_DIR}/wf-conv.txt
 WF_XSL=${TMP_DIR}/wf-xsl.txt
 WF_TEMP=${TMP_DIR}/wf-temp.txt
+ELEMENTS_XSL=${TMP_DIR}/element-xsl.txt
 
+# Extracts just the text content out of ODT and cnxml documents for diffing
 echo '
 <xsl:stylesheet version="1.0"
   xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
   xmlns:c="http://cnx.rice.edu/cnxml"
   xmlns:mml="http://www.w3.org/1998/Math/MathML"
   xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0"
+  xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0"
   >
   <xsl:template match="text()">
-    <xsl:value-of select="concat(&quot; &quot;, normalize-space(.), &quot; &quot;)"/>
-<!--
-    <xsl:value-of select="."/>
--->
+    <xsl:value-of select="normalize-space(.)"/>
   </xsl:template>
   <!-- Ignore "Untitled Document" -->
   <xsl:template match="c:document/c:title"/>
@@ -78,51 +81,89 @@ echo '
   <xsl:template match="mml:*"/>
   <!-- Just the text "formula" -->
   <xsl:template match="svg:desc"/>
+  <!-- Ignore change sets -->
+  <xsl:template match="text:changed-region|text:change-start|text:change-end"/>
 </xsl:stylesheet>
 ' > ${WF_XSL}
 
+# Prints every time an element occurs
+echo '
+<xsl:stylesheet version="1.0"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:c="http://cnx.rice.edu/cnxml"
+  xmlns:mml="http://www.w3.org/1998/Math/MathML"
+  >
+  <xsl:template match="c:*|mml:math">
+    <xsl:message>
+      <xsl:value-of select="name()"/>
+      <xsl:apply-templates select="@*"/>
+    </xsl:message>
+    <xsl:apply-templates select="node()"/>
+  </xsl:template>
+  <!-- Print out the attributes (except for @id and @xml:id) -->
+  <xsl:template match="@*">
+    <xsl:text> </xsl:text>
+    <xsl:value-of select="name()"/>
+    <xsl:text>="</xsl:text>
+    <xsl:value-of select="."/>
+    <xsl:text>"</xsl:text>
+  </xsl:template>
+
+  <!-- Attributes that only get a printed name (but not value) get listed below -->
+  <xsl:template match="@src|@document|@cols|@colname|@colnum|@url|@target-id">
+    <xsl:text> </xsl:text>
+    <xsl:value-of select="name()"/>
+  </xsl:template>
+  
+  <!-- Ignored attributes and elements get listed below -->
+  <xsl:template match="@id|@xml:id|@src"/>
+  <xsl:template match="node()|c:document|c:content">
+    <xsl:apply-templates select="node()"/>
+  </xsl:template>
+  
+  <!-- Uncomment this to exclude "simple" conversions -->
+  <xsl:template match="c:title|c:para|c:emphasis|c:list|c:item|c:table|c:tbody|c:tgroup|c:colspec|c:row|c:entry|c:link[@url]|c:figure|c:image|c:media">
+    <xsl:message>__misc__</xsl:message>
+    <xsl:apply-templates select="node()"/>
+  </xsl:template>
+</xsl:stylesheet>
+' > ${ELEMENTS_XSL}
+
+
+test ${BATCH} -ne 0 && echo "Filename \tErrors-or-Warnings \t#-Diffs \t%of-text-diff \tRNG-invalid"
+
+
 for f in $*
 do
-	
-	if [ "." != ".${2}" ]; then
-	if [ -e ${f}.xml ]; then
-		echo "Skipping ${f} because it was successfully converted"
-		continue
-	fi
-	fi
-	
-	echo "--------------------------"
-	echo "Starting ${f}"
-	echo "--------------------------"
+		
+  if [ ${BATCH} -eq 0 ]; then
+    echo "--------------------------"
+    echo "Starting ${f}"
+    echo "--------------------------"
+  fi
+  
+  # Batch-mode: Just print stats
+  test ${BATCH} -ne 0 && printf "${f}"
+
 
   if [ "odt" = $(echo ${f#*.}) ]; then
     ODT_FILE=${f}
   else
-  	#rm ${ODT_FILE}
     ${OOFFICE_BIN} -invisible "${OOFFICE_MACRO}(${f},${ODT_FILE})"
 
-    #[ 0 = $? ] || echo "ERROR: Could not convert document to Open Office" && continue
-    if [ 0 != $? ]; then
+    # If there was an error or the file didn't generate print error
+    if [ 0 != $? -o ! -s ${ODT_FILE} ]; then
       echo "ERROR: Could not convert document to Open Office"
       continue
     fi
   fi
 	
-  python odt2cnxml.py ${VERBOSE} ${ODT_FILE} ${TMP_DIR} > ${f}.xml
+  python odt2cnxml.py ${VERBOSE} ${ODT_FILE} ${TMP_DIR} > ${f}.xml 2> ${STDERR}
+  INVALID=$?
   
-  #if [ 0 != $? ]; then
-  if [ -e ${SCHEMA} ]; then
-    echo "INFO: Validating against Relax-NG Schema"
-    ${JING_BIN} ${SCHEMA} ${f}.xml
-
-  	if [ 0 != $? ]; then
-	    echo "ERROR: Invalid CNXML ${f}"
-	    mv ${f}.xml ${f}.xml.broken
-  	  continue
-	  fi
-	fi
-	#fi
-	
+  # Print the number of warnings/errors
+  test ${BATCH} -ne 0 && printf "\t$(cat ${STDERR} | wc -l)"
+  
   # So, here goes:
   # - Take all the text content from the original document
   # - Remove all spaces
@@ -131,46 +172,75 @@ do
   unzip -p ${ODT_FILE} content.xml | xsltproc ${WF_XSL} - | grep -o "[^\ ]\+" | tr -d '\n' | sed "s/\(.\)/\1\n/g" > ${WF_ORIG}
 	xsltproc ${WF_XSL} ${f}.xml | grep -o "[^\ ]\+" | tr -d '\n' | sed "s/\(.\)/\1\n/g" > ${WF_CONV}
 	
-	
+	DIFF_COUNT=0    # If there are no diffs then these are 0 by default
+	DIFF_PERCENT=0
 	if [ -s ${WF_ORIG} ]; then
-	if [ -s ${WF_CONF} ]; then
-	
+	if [ -s ${WF_CONV} ]; then
+    if [ $(cat ${WF_ORIG} | wc -l) -lt $(cat ${WF_CONV} | wc -l) ]; then
+      MAX_SIZE=$(cat ${WF_CONV} | wc -l)
+    else
+      MAX_SIZE=$(cat ${WF_ORIG} | wc -l)
+    fi
 	  
 	  diff ${WF_ORIG} ${WF_CONV} > ${WF_TEMP}
 
   	if [ -s ${WF_TEMP} ]; then
 
-      # When only running 1 conversion show the diff
-      if [ "." = ".${2}" ]; then
-        cat ${WF_TEMP}
-      fi
-  
       # The diff has entries like:
       # < h
       # < i
-      awk '
+      DIFF_COUNT=$(awk "
         BEGIN { }
-        /</     { orig += 1; acc1 = acc1 $2 }
-        />/     { conv += 1; acc2 = acc2 $2 }
-        /[0-9,cd]+/    { print acc1 " ]vs[ " acc2; acc1 = ""; acc2 = "" }
-        /---/   { acc1 = acc1 " "; acc2 = acc2 " " }
-        END   {
-                print acc1 " ]vs[ " acc2;
-                d  = orig  - conv;
-                # Skirt Divide by 0 Error
-                if(orig + conv == 0) {
-                  orig = 1;
-                }
-                print d, "=DIFFS", (d / (orig + conv)), "=DIFFS/LINEISH"
-        }
-      ' ${WF_TEMP}
+        /</         { orig += 1; acc1 = acc1 \$2 }
+        />/         { conv += 1; acc2 = acc2 \$2 }
+        /[0-9,cd]+/ {
+                      if(${BATCH} == 0) {
+                        print acc1 \" ]vs[ \" acc2 > \"/dev/stderr\";
+                      }
+                      acc1 = \"\"; acc2 = \"\";
+                    }
+        /---/       { acc1 = acc1 \" \"; acc2 = acc2 \" \" }
+        END         {
+                      if(${BATCH} == 0) {
+                        print acc1 \" ]vs[ \" acc2 > \"/dev/stderr\";
+                      }
+                      d  = orig  + conv;
+                      print d
+                    }
+      " ${WF_TEMP})
+      
+      DIFF_PERCENT=$(echo "scale=2;(${DIFF_COUNT} *100) / ${MAX_SIZE}" | bc)
     fi
 	fi
 	fi
 
-	echo "--------------------------"
-	echo "INFO: Success!!!!!"
-	echo "--------------------------"
+  test ${BATCH} -ne 0 && printf "\t${DIFF_COUNT}\t${DIFF_PERCENT}"
+  
+  test ${BATCH} -eq 0 && echo "DIFFS: " ${DIFF_COUNT} ${DIFF_PERCENT}
+
+  
+  test ${BATCH} -ne 0 -a ${INVALID} -ne 0 && printf "\tinvalid"
+  test ${BATCH} -ne 0 && echo "" #newline
+
+  # Validate!
+  if [ ${BATCH} -eq 0 -a -e ${SCHEMA} ]; then
+    echo "INFO: Validating against Relax-NG Schema"
+
+    ${JING_BIN} ${SCHEMA} ${f}.xml
+    VALID=$?
+
+  	if [ 0 != ${VALID} ]; then
+  	  echo "ERROR: Invalid CNXML ${f}"
+    else
+      echo "INFO: Success!!!!!"
+	  fi
+	fi
+	#fi
+
+  # Print out all the "uncommon" elements generated by the conversion
+  test ${BATCH} -eq 0 &&  xsltproc ${ELEMENTS_XSL} ${f}.xml 2>&1 | sort | uniq -c | sort -n -r
+
+  test ${BATCH} -ne 0 && rm ${f}.xml
 done
 
 if [ '.' != ".${2}" ]; then
