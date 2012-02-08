eaa9fe25fc9a287a9f62cdd6594646840a80328a
Roché Compaan <roche@upfrontsystems.co.za>
2012-02-08 20:29:13 +0200

    add support for validating with jing

--- rhaptos/cnxmlutils/validatecnxml.py
+++ rhaptos/cnxmlutils/validatecnxml.py
@@ -1,20 +1,37 @@
 import os
+import subprocess
+from tempfile import NamedTemporaryFile
 from lxml import etree
 
 dirname = os.path.dirname(__file__)
 
-def validate(cnxmlstr):
-    schemafile = open(os.path.join(dirname,
-                      'schema/cnxml/rng/0.7/cnxml.rng'))
-    relaxng_doc = etree.parse(schemafile)
-    relaxng = etree.RelaxNG(relaxng_doc)
-    cnxmldoc= etree.fromstring(cnxmlstr)
-    valid = relaxng.validate(cnxmldoc)
-    if valid:
-        print "Document validates against cnxml 0.7"
+def validate(cnxmlstr, validator='lxml'):
+    schemafn = os.path.join(dirname, 'schema/cnxml/rng/0.7/cnxml.rng')
+    if validator == 'lxml':
+        schemafile = open(schemafn)
+        relaxng_doc = etree.parse(schemafile)
+        relaxng = etree.RelaxNG(relaxng_doc)
+        cnxmldoc= etree.fromstring(cnxmlstr)
+        valid = relaxng.validate(cnxmldoc)
+        if valid:
+            print "Document validates against cnxml 0.7"
+        else:
+            print relaxng.error_log
+        return valid, relaxng.error_log
+    elif validator == 'jing':
+        tmpfile = NamedTemporaryFile() # doesn't take stdin, so make a file
+        tmpfile.write(cnxmlstr)
+        tmpfile.flush()
+        tmploc = tmpfile.name
+        cmdargs = ["jing", schemafn, tmploc]
+        process = subprocess.Popen(cmdargs,
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate(cnxmlstr)
+        valid = stdout is None
+        return valid, stdout
     else:
-        print relaxng.error_log
-    return valid, relaxng.error_log
+        raise RuntimeError("Unknown validator: %s" % validator)
+        
 
 def main():
     try:
