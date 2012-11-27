6edc4b25e8906d6e71f85864e9cfd96b99e95d89
Michael Mulich <michael.mulich@gmail.com>
2012-11-27 11:59:58 -0500

    the location of the file won't change during it's lifetime. Do in test imports to prevent ImportError failures.

--- rhaptos/cnxmlutils/tests/test_odt2cnxml.py
+++ rhaptos/cnxmlutils/tests/test_odt2cnxml.py
@@ -1,28 +1,30 @@
+# -*- coding: utf-8 -*-
 import os
 import re
 import unittest
 from lxml import etree
 
-from rhaptos.cnxmlutils.odt2cnxml import transform
-
-dirname = os.path.abspath(os.path.dirname(__file__))
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 class TestTransform(unittest.TestCase):
 
     def test_bolditalic(self):
-        odtfile = os.path.join(dirname, 'data', 'bolditalic.odt')
-        valid_cnxmlfile = os.path.join(dirname, 'data', 'bolditalic.cnxml')
-        with open(valid_cnxmlfile) as f:
+        odtfile = os.path.join(HERE, 'data', 'bolditalic.odt')
+        valid_cnxmlfile = os.path.join(HERE, 'data', 'bolditalic.cnxml')
+        with open(valid_cnxmlfile, 'r') as f:
             validcnxml = f.read()
-        # remove newlines and indentation and ids
+
+        # Remove newlines and indentation and ids
         validcnxml = validcnxml.replace('\n', '')
         validcnxml = re.sub('>\s+<', '><', validcnxml)
         validcnxml = re.sub('id=\".*?\"', '', validcnxml)
+        # Encode to utf-8 because that's what etree.tostring outputs.'
         validcnxml = validcnxml.encode('utf-8')
 
+        from rhaptos.cnxmlutils.odt2cnxml import transform
         cnxml, images, errors = transform(odtfile)
         cnxml = etree.tostring(cnxml)
-        # strip ids
+        # Strip ids, because (they differ each run?).
         cnxml = re.sub(b'id=\".*?\"', b'', cnxml)
         self.assertEqual(cnxml, validcnxml)
