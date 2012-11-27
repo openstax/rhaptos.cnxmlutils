530c973e62a9d341362477bb0a01334cb59f20f7
Michael Mulich <michael.mulich@gmail.com>
2012-11-27 12:52:19 -0500

    Import reorganization and argparse is in the standard library.

--- rhaptos/cnxmlutils/html2cnxml.py
+++ rhaptos/cnxmlutils/html2cnxml.py
@@ -6,39 +6,30 @@ The results are sent to standard out.
 
 """
 import sys
-try:
-    from io import StringIO
-except ImportError:
-    from io import StringIO
+import argparse
+from io import StringIO
+
 import pkg_resources
 from lxml import etree
-from rhaptos.cnxmlutils.utils import html_to_cnxml
+from .utils import html_to_cnxml
 
 
 def main():
-    try:
-        import argparse
-    except ImportError:
-        print("The 'argparse' distribution is needed "
-              "to run from the commandline")
-        print("Recommendation: Use the 'cmdline_support'' requirement extra.")
-        return 2
-
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('document', nargs=1,
+    parser.add_argument('document',
                         type=argparse.FileType('r'),
                         default=sys.stdin,
                         help="the filesystem location of the html document")
-    parser.add_argument('cnxml_document', nargs=1,
+    parser.add_argument('cnxml_document',
                         type=argparse.FileType('r'),
                         help="the existing CNXML document")
     args = parser.parse_args()
 
-    result = html_to_cnxml(args.document[0], args.cnxml_document[0])
+    result = html_to_cnxml(args.document, args.cnxml_document)
 
     print(result)
-
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
