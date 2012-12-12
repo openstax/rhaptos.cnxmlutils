c50b22400c16614e31fb48dbc1f8106427a04e23
Marvin Reimer <therealmarv@gmail.com>
2012-12-12 18:01:08 +0100

    start with Aloha's HTML to more structured HTML

---
+++ rhaptos/cnxmlutils/aloha2html.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Converts HTML5 from Aloha to a more structured HTML5 where:
+- Headers are sections
+- Mathjax will be replaced with real MathML
+The results are sent to standard out.
+"""
+
