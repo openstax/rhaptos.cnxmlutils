236f127b0c0f39fc5838e315adc58292f9a2ed73
Marvin Reimer <therealmarv@gmail.com>
2012-12-15 19:20:29 +0100

    include tidy-html5 in processing

--- rhaptos/cnxmlutils/utils.py
+++ rhaptos/cnxmlutils/utils.py
@@ -10,6 +10,7 @@ except ImportError:
 import pkg_resources
 from lxml import etree
 from functools import partial
+from tidylib import tidy_document # requires tidy-html5 from https://github.com/w3c/tidy-html5 Installation: http://goo.gl/FG27n
 
 __all__ = (
     'NAMESPACES', 'XHTML_INCLUDE_XPATH', 'XHTML_MODULE_BODY_XPATH',
@@ -23,8 +24,42 @@ XHTML_INCLUDE_XPATH = etree.XPath('//xhtml:a[@class="include"]',
                                   namespaces=NAMESPACES)
 XHTML_MODULE_BODY_XPATH = etree.XPath('//xhtml:body', namespaces=NAMESPACES)
 
+# Tidy up the Google Docs HTML Soup
+def _tidy2xhtml5(html):
+    """Tidy up a html4/5 soup to a parsable valid XHTML5.
+    Requires tidy-html5 from https://github.com/w3c/tidy-html5 Installation: http://goo.gl/FG27n
+    """
+    #import pdb;pdb.set_trace()
+    html = _io2string(html)
+    xhtml5, errors = tidy_document(html,
+        options={
+            'output-xml': 1,       # create xml output
+            'indent': 0,           # Don't use indent, add's extra linespace or linefeeds which are big problems
+            'tidy-mark': 0,        # No tidy meta tag in output
+            'wrap': 0,             # No wrapping
+            'alt-text': '',        # Help ensure validation
+            'doctype': 'strict',   # Little sense in transitional for tool-generated markup...
+            'force-output': 1,     # May not get what you expect but you will get something
+            'numeric-entities': 1, # remove HTML entities like e.g. nbsp
+            'clean': 1,            # remove
+            'bare': 1,
+            'word-2000': 1,
+            'drop-proprietary-attributes': 1,
+            'enclose-text': 1,     # enclose text in body always with <p>...</p>
+            'logical-emphasis': 1, # transforms <i> and <b> text to <em> and <strong> text
+            'doctype': 'html5',
+            })
+    #print errors # for debugging
+    return xhtml5
 
-def _to_io_object(s):
+def _io2string(s):
+    """If necessary it will convert the io object to an string
+    """
+    if hasattr(s, 'read'):
+        s = s.read()
+    return s
+
+def _string2io(s):
     """If necessary it will convert the string to an io object
     (e.g. with read and write methods).
     """
@@ -46,13 +81,17 @@ def _make_xsl(filename):
 def _transform(xsl_filename, xml):
     """Transforms the xml using the specifiec xsl file."""
     xslt = _make_xsl(xsl_filename)
-    print xsl_filename
     xml = xslt(xml)
     return xml
 
+def _transform_string(xsl_filename, xml_string):
+    source = _string2io(xml_string)
+    xml = etree.parse(source)
+    return etree.tostring(_transform(xsl_filename, xml))
+
 def cnxml_to_html(cnxml_source):
     """Transform the CNXML source to HTML"""
-    source = _to_io_object(cnxml_source)
+    source = _string2io(cnxml_source)
     xml = etree.parse(source)
     # Run the CNXML to HTML transform
     xml = _transform('cnxml-to-html5.xsl', xml)
@@ -61,27 +100,27 @@ def cnxml_to_html(cnxml_source):
 
 
 ALOHA2HTML_TRANSFORM_PIPELINE = [
-    partial(_transform, 'aloha-to-html5-pass01-leveled-headers.xsl'),
-    partial(_transform, 'aloha-to-html5-pass02-new-min-header-level.xsl'),
-    partial(_transform, 'aloha-to-html5-pass03-nested-headers.xsl'),
-    partial(_transform, 'aloha-to-html5-pass04-headers2sections.xsl'),
+    _tidy2xhtml5,
+    partial(_transform_string, 'aloha-to-html5-pass01-leveled-headers.xsl'),
+    partial(_transform_string, 'aloha-to-html5-pass02-new-min-header-level.xsl'),
+    partial(_transform_string, 'aloha-to-html5-pass03-nested-headers.xsl'),
+    partial(_transform_string, 'aloha-to-html5-pass04-headers2sections.xsl'),
 ]
 
 def aloha_to_html(html_source):
     """Converts HTML5 from Aloha to a more structured HTML5"""
-    source = _to_io_object(html_source)
-    xml = etree.parse(source)
+    xml = html_source
     for i, transform in enumerate(ALOHA2HTML_TRANSFORM_PIPELINE):
         xml = transform(xml)
-    return etree.tostring(xml, pretty_print=True)
+    return xml
 
 def html_to_cnxml(html_source, cnxml_source):
     """Transform the HTML to CNXML. We need the original CNXML content in
     order to preserve the metadata in the CNXML document.
     """
-    source = _to_io_object(html_source)
+    source = _string2io(html_source)
     xml = etree.parse(source)
-    cnxml = etree.parse(_to_io_object(cnxml_source))
+    cnxml = etree.parse(_string2io(cnxml_source))
     # Run the HTML to CNXML transform on it
     xml = _transform('html5-to-cnxml.xsl', xml)
     # Replace the original content element with the transformed one.
