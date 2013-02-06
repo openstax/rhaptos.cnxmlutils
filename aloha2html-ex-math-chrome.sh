f6f0d7942e46d348c7bfe0009908cbe97b68f24f
Marvin Reimer <therealmarv@gmail.com>
2013-02-06 12:12:39 -0600

    add example scripts for aloha2html and html2validcnxml

---
+++ aloha2html-ex-math-chrome.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+python -m rhaptos.cnxmlutils.aloha2html ./rhaptos/cnxmlutils/tests/data/aloha2html-gdocs-htmlsoup-with-math-example-chrome.html
---
+++ html2validcnxml-ex-1.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+python -m rhaptos.cnxmlutils.html2validcnxml ./rhaptos/cnxmlutils/tests/data/html2validcnxml1.html
