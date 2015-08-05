4a7a93c3052801039d94c1359bbe59154e23b762
Michael Mulich <michael.mulich@gmail.com>
2015-08-04 17:04:35 -0700

    Fix installation manifest and include the lxml dependency for >= py2.7

--- setup.py
+++ setup.py
@@ -7,9 +7,19 @@ Copyright (C) 2013 Rice University
 This software is subject to the provisions of the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).  
 See LICENSE.txt for details.
 """
-
-from setuptools import setup, find_packages
 import os
+import sys
+from setuptools import setup, find_packages
+
+IS_PY24 = sys.version_info < (2, 7,)
+
+install_requires = [
+    'setuptools',
+    ]
+if not IS_PY24:
+    # Only list lxml as a dependency when outside the legacy context,
+    # which is one that isn't running python >= 2.7.
+    install_requires.append('lxml')
 
 version = '1.2'
 
@@ -33,13 +43,5 @@ setup(name='rhaptos.cnxmlutils',
       include_package_data=True,
       zip_safe=False,
       test_suite='rhaptos.cnxmlutils.tests',
-      install_requires=[
-          'setuptools',
-      #    'lxml',
-          #'argparse',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      install_requires=install_requires,
       )
